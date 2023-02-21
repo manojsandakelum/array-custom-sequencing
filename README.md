b0c4a561598410552578a2dae891317a017c0687
manoj.sandakelum <manoj.sandakelum@CSI-MANOJS>
2023-02-21 17:32:49 +0530

    Initial commit

--- README.md
+++ README.md
@@ -1,4 +1,5 @@
 # ArrayCustomSequencingAngular
+Move an array element from one array position to another
 
 This project was generated with [Angular CLI](https://github.com/angular/angular-cli) version 7.1.0.
 
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,22 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'array-custom-sequencing-angular';
+
+
+  dataArray = [{"name":"Sam" , "sequence":1},
+               {"name":"Ran" , "sequence":2},
+               {"name":"Ban" , "sequence":3},
+               {"name":"Dam" , "sequence":4},
+               {"name":"Nam" , "sequence":5}]
+
+  move(dataArray,new_index,old_index){
+    dataArray.splice(new_index, 0, dataArray.splice(old_index, 1)[0]);
+    let i= 0;
+    dataArray.forEach(element => {
+      i++
+      element.sequence = i;
+    });
+    return dataArray;
+  } 
+
 }
